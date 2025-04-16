00aa2f8a311681eec7707230dd487e9b71ffd888
Imène TOUDEFT <imene.toudeft@hotmail.com>
2025-04-16 15:00:55 +0200

    add script for ennemies appearance

---
+++ Assets/TutorialInfo/Scripts/Ops Appearence.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class OpsApparition : MonoBehaviour
+{
+    public GameObject opsPrefab; 
+    public float spawnInterval = 2f; 
+    public float spawnDistance = 10f;
+    public float opsSpeed = 2f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnOps", 2f, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void SpawnOps()
+    {
+        Vector3 spawnDirection = Random.onUnitSphere;
+        spawnDirection.y = 0f; // Flatten direction to horizontal plane
+        spawnDirection.Normalize(); // Ensure it's still a unit vector
+        
+        Vector3 spawnPosition = transform.position + spawnDirection * spawnDistance;
+        spawnPosition.y = transform.position.y; // Lock Y to player's Y
+        
+        GameObject ops = Instantiate(opsPrefab, spawnPosition, Quaternion.identity);
+        Rigidbody rb = ops.GetComponent<Rigidbody>();
+        
+        if (rb != null)
+        {
+            //rb.linearVelocity = (transform.position - spawnPosition).normalized * opsSpeed;
+            rb.linearVelocity = (transform.position - spawnPosition).normalized * opsSpeed;
+        }
+    }
+}
